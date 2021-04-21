ef72e31cda9cd3226db40b245e78ce781e5a3be6
Yaroslav <yakravets@ktc.rovno.ua>
2021-04-21 12:44:37 +0300

    add room.view

--- Room/Rooms.API/Models/RoomDTO.cs
+++ Room/Rooms.API/Models/RoomDTO.cs
@@ -7,10 +7,13 @@ namespace Rooms.API.Models
 {
     public class RoomDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Duration { get; set; }
         public int MinCountGamers { get; set; }        
         public int MinAge { get; set; }
         public double Rating { get; set; }
+        
+        public string PictureUrl { get; set; }
     }
 }
--- Room/Rooms.API/Models/RoomDetailDTO.cs
+++ Room/Rooms.API/Models/RoomDetailDTO.cs
@@ -19,5 +19,7 @@ namespace Rooms.API.Models
         public string Company { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+        
+        public string PictureUrl { get; set; }
     }
 }
